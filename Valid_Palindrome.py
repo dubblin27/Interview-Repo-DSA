0b940b61da9b6027a95c504fb43e9989e58f3c53
Sabrish Kumar P <58567129+dubblin27@users.noreply.github.com>
2020-07-24 00:59:47 +0530

    initial commit

---
+++ Valid_Palindrome.py
@@ -0,0 +1,18 @@
+#FB, APPLE
+#Link : https://leetcode.com/problems/valid-palindrome/description/ 
+
+# Example 1:
+
+# Input: "A man, a plan, a canal: Panama"
+# Output: true
+
+# Example 2:
+
+# Input: "race a car"
+# Output: false
+
+ 
+
+# Constraints:
+#     s consists only of printable ASCII characters.
+
