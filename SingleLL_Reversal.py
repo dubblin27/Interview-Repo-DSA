0e34f122c5191b6d4eba5851d8e6d9e659e07db7
Sabrish Kumar P <58567129+dubblin27@users.noreply.github.com>
2020-07-24 01:42:13 +0530

    initial commit

---
+++ SingleLL_Reversal.py
@@ -0,0 +1,14 @@
+#Google, FB, Amazon 
+# Link : https://leetcode.com/problems/reverse-linked-list/ 
+
+# Example:
+
+# Input: 1->2->3->4->5->NULL
+# Output: 5->4->3->2->1->NULL
+
+# Input: NULL
+# Output: NULL
+
+# Follow up:
+
+# A linked list can be reversed either iteratively or recursively. Could you implement both?
